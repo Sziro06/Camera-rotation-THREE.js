b1d7f2f93511255cfbf543512d87778c93d694e0
Sziro06 <huoea071@gmail.com>
2023-09-10 12:10:32 +0200

    Update
    I can rotate my camera to up, down, left and right, and now I want to rotate my camera in 3D.

---
+++ main.js
@@ -0,0 +1,42 @@
+let xSpeed = 0.0001;
+let ySpeed = 0.0001;
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGL1Renderer();
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1);
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame( animate );
+
+
+
+	renderer.render( scene, camera );
+}
+
+document.addEventListener("keydown", onDocumentKeyDown, false);
+
+function onDocumentKeyDown(event) {
+    let keyCode = event.which;
+    if (keyCode === 68 || keyCode === 100) { // A eller a
+        camera.rotation.y += 0.01;
+    } else if (keyCode === 65 || keyCode === 97) { // D eller d
+        camera.rotation.y -= 0.01; // Endret retning for høyre rotasjon
+    } else if (keyCode === 87 || keyCode === 119) { // W eller w
+        camera.rotation.x -= 0.01;
+    } else if (keyCode === 83 || keyCode === 115) { // S eller s
+        camera.rotation.x += 0.01;
+    }
+}
+
+
+animate();
