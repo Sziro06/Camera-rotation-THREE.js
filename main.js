1d140dcf5c2c9e2b710fbc4af40ee7740cfe962e
Sziro06 <huoea071@gmail.com>
2023-09-13 22:42:13 +0200

    update

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-const scene = new THREE.Scene();
-const renderer = new THREE.WebGL1Renderer();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+let scene, renderer, camera, zoom;
+
+scene = new THREE.Scene();
+renderer = new THREE.WebGL1Renderer();
+zoom = 1000;
+camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, zoom );
 
 camera.position.z = 4.5;
 camera.position.y = 1;
-
-
 
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
@@ -72,15 +73,13 @@ document.addEventListener("keydown", onDocumentKeyDown, false);
 function onDocumentKeyDown(event) {
     let keyCode = event.which;
     if (keyCode === 68 || keyCode === 100) { // D eller f
-        camera.rotation.y -= 0.1;
+        camera.rotation.y -= 0.01;
     } else if (keyCode === 65 || keyCode === 97) { // A eller a
-        camera.rotation.y += 0.1;
-    } else if (keyCode === 87 || keyCode === 119) { // S eller s
-        camera.rotation.x += 0.1;
-    } else if (keyCode === 83 || keyCode === 115) { // W eller w
-        camera.rotation.x -= 0.1;
-    // } else if (keyCode === 82 || keyCode === 114) { // S eller s
-    //     camera.rotation.z -= 0.01;
+        camera.rotation.y += 0.01;
+    } else if (keyCode === 87 || keyCode === 119) { // W eller w
+        zoom = zoom - 10;
+    } else if (keyCode === 83 || keyCode === 115) { // S eller s
+        camera.rotation.x -= 0.01;
     }
 }
 
